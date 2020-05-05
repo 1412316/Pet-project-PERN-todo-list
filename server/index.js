b112fa84db435991bd602b673cfad1705a4a4aae
1412316 <nhatminhdang1996@gmail.com>
2020-05-05 20:59:44 +0700

    Failed to deploy

--- server/index.js
+++ server/index.js
@@ -76,6 +76,7 @@ app.delete("/todos/:id", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
+var port = process.env.PORT || 5000;
+app.listen(port, () => {
   console.log("Server has started on port 5000");
 });
